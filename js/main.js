c7fc65fd92aa35418c8b8d15867217c88d5ec1fd
João Henrique Muller Bail <107154001+joaoomuller@users.noreply.github.com>
2024-05-09 19:57:27 -0300

    Animação de árvore

--- js/main.js
+++ js/main.js
@@ -3,9 +3,9 @@ import * as climate from "./climate-control.js"
 import * as nature from "./nature-control.js"
 
 function init() {
-    hands.addHandsListeners();
     climate.showDay();
     nature.generateTrees();
+    hands.addHandsListeners();
 }
 
 window.addEventListener('DOMContentLoaded', init);
